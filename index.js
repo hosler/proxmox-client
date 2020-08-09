4b7fea2402ebf0c1f1ec5bc8e6173e2ee7016ee4
Benjamin Grziwok <bitbucket@beefo.io>
2020-08-09 19:15:55 +0200

    * add debugger * update authorization * accept object as body and set right content-type

--- index.js
+++ index.js
@@ -1,74 +1,94 @@
-const http = require('superagent');
+const http = require('superagent'),
+      debug = require('debug'),
+      log = debug('proxmox:log'),
+      error = debug('proxmox:error'),
+      logHttp = debug('proxmox:http');
 
 class Proxmox {
   constructor() {
     this.baseUrl = '';
-    this.user = '';
-    this.tokenName = '';
+    this.tokenInfo = '';
     this.token = '';
-    this.realm = '';
     this.Proxmox = Proxmox;
   }
 
   auth(host, user, tokenName, token, realm = 'pam') {
+    log('got authorization details');
     this.baseUrl = `https://${host}/api2/json`;
-    this.user = user;
-    this.tokenName = tokenName;
     this.token = token;
-    this.realm = realm;
+    if(!token || !token.length) {
+      this.tokenInfo = user;
+      this.token = tokenName;
+      return;
+    }
+    this.tokenInfo = `${user}@${realm}!${tokenName}`;
+    return;
   }
 
   async call(method, path, body) {
-    method = method.toLocaleLowerCase();
+    method = method.toLocaleUpperCase()
     var req = http[method](`${this.baseUrl}${path}`);
-    req.set('Authorization', `PVEAPIToken=${this.user}@${this.realm}!${this.tokenName}=${this.token}`);
+    var contentType = 'application/x-www-form-urlencoded';
+    req.set('Authorization', `PVEAPIToken=${this.tokenInfo}=${this.token}`);
     if(body == undefined) {
-      body = '';
+      body = {};
     }
-    if(method !== 'get' &&  method !== 'delete') {
-      req.set('Content-Type', 'application/x-www-form-urlencoded');
+    if(typeof body == 'string') {
+      try {
+        body = JSON.parse(body);
+      } catch(e) {
+
+      }
     }
+    if(typeof body == 'object') {
+      body = JSON.stringify(body);
+      coontentType = 'application/json';
+    }
+    if(method == 'POST' &&  method == 'PUT') {
+      req.set('Content-Type', contentType);
+    }
+    logHttp(`sending ${method} request to ${this.baseUrl}${path}`);
     req.send(body).catch();
-	if(req === undefined || req === "undefined") {
-		req = { data: "error" }
-	}
+    if(req === undefined || req === "undefined") {
+      req = { data: "error" }
+    }
     return req;
   }
 
   async get(path) {
-   var response =	await this.call('GET', path, '');
-   if(typeof response === undefined || response === "undefined") {
-		response = { data: "error" }
-	}
+    var response =	await this.call('GET', path, '');
+    if(typeof response === undefined || response === "undefined") {
+      response = { data: "error" }
+    }
    return response
   }
   async post(path, body) {
-   var response =	await this.call('POST', path, body);
-   if(typeof response === undefined || response === "undefined") {
-		response = { data: "error" }
-	}
+    var response =	await this.call('POST', path, body);
+    if(typeof response === undefined || response === "undefined") {
+      response = { data: "error" }
+    }
    return response
   }
   async put(path, body) {
-	var response = await this.call('PUT', path, body);
-	if(typeof response === undefined || response === "undefined") {
-		response = { data: "error" }
-	}
-	return response
+    var response = await this.call('PUT', path, body);
+    if(typeof response === undefined || response === "undefined") {
+      response = { data: "error" }
+    }
+    return response
   }
   async delete(path) {
-	var response = await this.call('DELETE', path, '');
-	if(typeof response === undefined || response === "undefined") {
-		response = { data: "error" }
-	} 
-	return response
+    var response = await this.call('DELETE', path, '');
+    if(typeof response === undefined || response === "undefined") {
+      response = { data: "error" }
+    } 
+    return response
   }
   async del(path) {
-	var response = await this.call('DELETE', path, '');
-	if(typeof response === undefined || response === "undefined") {
-		response = { data: "error" }
-	} 
-	    return response
+    var response = await this.call('DELETE', path, '');
+    if(typeof response === undefined || response === "undefined") {
+      response = { data: "error" }
+    } 
+    return response
   }
 }
 
